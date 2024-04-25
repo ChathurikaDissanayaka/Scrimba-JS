309f1e09b4a9990638cdf88146f388b7dda84954
Sachini <sachichathudissanayaka@gmail.com>
2024-04-25 22:04:55 +0530

    add changes

--- Blackjack Game/index.js
+++ Blackjack Game/index.js
@@ -6,19 +6,23 @@ let message = "";
 
 let sum = firstCard + secondCard;
 
-// 2. Create a startGame() function. Move the conditional
-// below (line 11-20) inside the body of the function.
+// 1. Store the message-el paragraph in a variable called messageEl
+let messageEl = document.getElementById("message-el")
 
 function startGame(){
     if (sum <= 20) {
-        message = "Do you want to draw a new card? 🙂";
+        message = "Do you want to draw a new card?";
+        // 🙂
     } else if (sum === 21) {
-        message = "Wohoo! You've got Blackjack! 🥳";
+        message = "You've got Blackjack!";
+        // 🥳
         hasBlackJack = true;
     } else {
-        message = "You're out of the game! 😭"
+        message = "You're out of the game!"
+        // 😭
         isAlive = false;
     }
     
-    console.log(message);
+    // 2. Display the message in the messageEl using messageEl.textContent
+    messageEl.textContent = message
 }
