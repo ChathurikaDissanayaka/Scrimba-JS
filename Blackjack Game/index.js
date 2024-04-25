61103a275106656ea95b6bda1070e5a7429792a2
Sachini <sachichathudissanayaka@gmail.com>
2024-04-25 21:43:17 +0530

    Add changes

--- Blackjack Game/index.js
+++ Blackjack Game/index.js
@@ -2,18 +2,23 @@ let firstCard = 4;
 let secondCard = 8;
 let hasBlackJack = false;
 let isAlive =  true;
-let message;
+let message = "";
 
 let sum = firstCard + secondCard;
 
-if (sum <= 20) {
-    message = "Do you want to draw a new card? 🙂";
-} else if (sum === 21) {
-    message = "Wohoo! You've got Blackjack! 🥳";
-    hasBlackJack = true;
-} else {
-    message = "You're out of the game! 😭"
-    isAlive = false;
-}
+// 2. Create a startGame() function. Move the conditional
+// below (line 11-20) inside the body of the function.
 
-console.log(message);
+function startGame(){
+    if (sum <= 20) {
+        message = "Do you want to draw a new card? 🙂";
+    } else if (sum === 21) {
+        message = "Wohoo! You've got Blackjack! 🥳";
+        hasBlackJack = true;
+    } else {
+        message = "You're out of the game! 😭"
+        isAlive = false;
+    }
+    
+    console.log(message);
+}
