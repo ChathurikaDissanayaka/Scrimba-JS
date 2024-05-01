1164e129d63f07d6e9443da54fefd94be40a5e39
Sachini <sachichathudissanayaka@gmail.com>
2024-05-01 21:31:22 +0530

    dd changes

--- Chrome Extension/index.js
+++ Chrome Extension/index.js
@@ -1,11 +1,8 @@
 let myLeads = []
 const inputEl = document.getElementById("input-el")
-
-// Push the value "www.awesomelead.com" to myArray when the input button is clicked
-
 const inputBtn = document.getElementById("input-btn")
 
 inputBtn.addEventListener("click", function(){
-    myLeads.push("www.awesomelead.com")
+    myLeads.push(inputEl.value)
     console.log(myLeads)
 })
