911328658e52e9641cf999f251fed820ccaad687
Sachini <sachichathudissanayaka@gmail.com>
2024-05-08 22:57:09 +0530

    add changes

--- Mobile App/index.js
+++ Mobile App/index.js
@@ -1,8 +1,3 @@
-/*
-Challenge:
-Make it so that when you click the 'Add to cart' button, whatever is written in the input field should be console logged.
-*/
-
 let inputField = document.getElementById("input-field")
 let addButton = document.getElementById("add-button")
 
