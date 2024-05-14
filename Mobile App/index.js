76d3b52d243f857b3d639e16b0b5f9b08230d3bf
Sachini <sachichathudissanayaka@gmail.com>
2024-05-15 00:05:07 +0530

    add changes

--- Mobile App/index.js
+++ Mobile App/index.js
@@ -1,6 +1,19 @@
-let inputField = document.getElementById("input-field")
+import {initializeApp} from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js"
+import {getDatabase, ref, push} from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js"
+
+const appSettings = {
+    databaseURL: "https://playground-8eb31-default-rtdb.asia-southeast1.firebasedatabase.app/"
+}
+
+const app = initializeApp(appSettings)
+const database = getDatabase(app)
+const shoppingListInDB = ref(database, "shoppingList")
+
+let inputFieldEl = document.getElementById("input-field")
 let addButton = document.getElementById("add-button")
 
 addButton.addEventListener("click", function(){
-    console.log(inputField.value)
+    let inputValue = inputFieldEl.value
+    push(shoppingListInDB, inputValue)
+    console.log(inputValue)
 })
