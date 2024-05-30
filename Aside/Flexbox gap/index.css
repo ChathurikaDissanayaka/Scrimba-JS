e8153325d9c6a1bb0562bb28b1003f2085d2fa81
Sachini <sachichathudissanayaka@gmail.com>
2024-05-30 22:19:34 +0530

    Add changes

---
+++ Aside/Flexbox gap/index.css
@@ -0,0 +1,17 @@
+html, body {
+    margin: 0;
+    padding: 0;
+    background-color: #000;
+}
+
+.beatles {
+    display: flex;
+    max-width: 440px;
+    margin: 30px auto;
+    flex-wrap: wrap;
+    gap: 10px;
+}
+
+.beatle {
+    border: 2px solid white;
+}
