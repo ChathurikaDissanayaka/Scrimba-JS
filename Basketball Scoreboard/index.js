7446b9703d17ba37cc961bce952d1a474c527f42
Sachini <sachichathudissanayaka@gmail.com>
2024-08-18 22:19:39 +0530

    Add changes

--- Basketball Scoreboard/index.js
+++ Basketball Scoreboard/index.js
@@ -4,14 +4,6 @@ let scoreR = 0;
 let score1 = document.getElementById("score-1");
 let score2 = document.getElementById("score-2");
 
-let btnl1 = document.getElementById("btn-l1");
-let btnl2 = document.getElementById("btn-l2");
-let btnl3 = document.getElementById("btn-l3");
-
-let btnr1 = document.getErementById("btn-r1");
-let btnr2 = document.getErementById("btn-r2");
-let btnr3 = document.getErementById("btn-r3");
-
 function addOneLeft(){
     scoreL += 1;
     score1.innerText = scoreL;
